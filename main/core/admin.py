37185d3990ec31920c983d172f2173fb3269bfa0
safisher <safisher@users.noreply.github.com>
2024-10-23 20:18:16 +0100

    stop models from being sorted in admin main index.

--- main/core/admin.py
+++ main/core/admin.py
@@ -24,31 +24,29 @@ from .models import (
     Slides,
 )
 
-
+# we can remove models from this list, if we don't want them to show in the admin index.
+# we use get_app_list() below to stop them from being sorted and rather they are listed
+# in the order they are added with admin.site.register().
 my_models = [
-    Species,
-    Organs,
-    OrganRegions,
-    MaterialSources,
-    People,
-    StainingProtocols,
-    ProbeTypes,
+    Donor,
+    Microscope,
     FishTechnologies,
     FlourescentMolecules,
-    ProbePanels,
     ImagingSuccessOptions,
-    # Probe,
-    # Panel,
-    Microscope,
-    # ExposureTime,
-    Donor,
-    # Slides,
+    MaterialSources,
+    Organs,
+    OrganRegions,
+    People,
+    ProbePanels,
+    ProbeTypes,
+    Species,
+    StainingProtocols,
 ]
 admin.site.register(my_models)
 
 
 class ProbeAdmin(admin.ModelAdmin):
-    list_display = ["name", "target_analyte", "probe_type"]
+    list_display = ["name", "target_analyte", "probe_type", "fluorescent_molecule"]
 
 
 admin.site.register(Probe, ProbeAdmin)
@@ -90,7 +88,7 @@ admin.site.register(ExposureTime, ExposureTimeAdmin)
 
 
 class SlidesAdmin(admin.ModelAdmin):
-    list_display = ["name", "species", "organ"]
+    list_display = ["name", "species", "organ", "staining_protocol"]
 
     fieldsets = [
         ("Origin", {"fields": ["name", "species", "organ", "organ_region", "donor"]}),
@@ -122,3 +120,17 @@ class SlidesAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Slides, SlidesAdmin)
+
+
+def get_app_list(self, request, app_label=None):
+    """Return the installed apps that have been registered in admin.py.
+    https://forum.djangoproject.com/t/reordering-list-of-models-in-django-admin/5300/9"""
+    app_dict = self._build_app_dict(request, app_label)
+    app_list = list(app_dict.values())
+    # Do not sort models alphatbetically
+    # for app in app_list:
+    #    app["models"].sort(key=lambda x: x["name"])
+    return app_list
+
+
+admin.AdminSite.get_app_list = get_app_list
