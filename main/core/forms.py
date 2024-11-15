78b3d06299c857096f7d4023acc4ecf9be26a80f
safisher <safisher@users.noreply.github.com>
2024-11-15 14:56:39 +0000

    cleaning up

--- main/core/forms.py
+++ main/core/forms.py
@@ -1,15 +1,1 @@
 from django import forms
-
-"""
-# currently unused. was used in admin.py
-class GetForeignKeyName(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return "{}".format(obj.name)
-"""
-
-"""
-# currently unused. was used in admin.py
-class GetManyToManyName(forms.ModelMultipleChoiceField):
-    def label_from_instance(self, obj):
-        return "{}".format(obj.name)
-"""
